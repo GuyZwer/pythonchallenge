5dc8bc92dc1ac9970dbcb674880e9eb8c6015a23
GuyZwer <guy.zwerdling@gmail.com>
2017-06-02 17:51:09 +0300

    change challenge 3 file

--- pythonchallenge/challenge3.py
+++ pythonchallenge/challenge3.py
@@ -1,15 +1,22 @@
 checkfile = open("challenge3file.txt", 'r')
 file = checkfile.read()
-print file
+file2 = file.replace('\n', '')
+print file2
+
 count = 0
+
+def checkletters(letters1, letters2):
+    if letters1 == letters2:
+        return True
 
 list1 = []
 for lop in file:
     count += 1
     if file[count-1].islower():
-        print file[count-1]
         if file[count:count+3].isupper() and file[count-4:count-1].isupper():
-            print file[count-4:count+3]
-            list1.append(file[count-1])
+            print 'yes'
+            if checkletters(file[count:count+3], file[count-4:count+1]):
+                print file[count-4:count+3]
+                list1.append(file[count-1])
 
 print ''.join(list1)

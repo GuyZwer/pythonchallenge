1703036dddad6a476439306e1511892722b5991f
GuyZwer <guy.zwerdling@gmail.com>
2017-06-12 13:07:14 +0300

    update

--- pythonchallenge/challenge3.py
+++ pythonchallenge/challenge3.py
@@ -9,14 +9,17 @@ def checkletters(letters1, letters2):
     if letters1 == letters2:
         return True
 
+def check3let(letters):
+    len = len(letters)
+
 list1 = []
-for lop in file:
+for lop in file2:
     count += 1
-    if file[count-1].islower():
-        if file[count:count+3].isupper() and file[count-4:count-1].isupper():
-            print 'yes'
-            if checkletters(file[count:count+3], file[count-4:count+1]):
-                print file[count-4:count+3]
-                list1.append(file[count-1])
+    if file2[count-1].islower():
+        if file2[count:count+3].isupper() and file2[count-4:count-1].isupper():
+            if checkletters(file2[count:count+3], file2[count-4:count+1]):
+                print 'yes'
+                print file2[count-4:count+3]
+                list1.append(file2[count-1])
 
 print ''.join(list1)
